a1620ae975564b2cc1a2f976a378581ca82afd93
rogeriowiethornjr <36937774+rogeriowiethornjr@users.noreply.github.com>
2020-10-24 23:37:30 -0300

    Update ImageScrollView.swift

--- Sources/ImageScrollView.swift
+++ Sources/ImageScrollView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 @objc public protocol ImageScrollViewDelegate: UIScrollViewDelegate {
     func imageScrollViewDidChangeOrientation(imageScrollView: ImageScrollView)
